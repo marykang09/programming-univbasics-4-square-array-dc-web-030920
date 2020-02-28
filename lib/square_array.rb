3326b21d14b422bd2951bbe7f89328a9f67b4dad
Mary Kang <mary.kang09@gmail.com>
2020-02-28 02:24:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    new_array << counter**1
+    new_array << counter**
     counter += 1
   end
   return new_array
